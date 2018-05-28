710cb18b8337b03dbebd6f7548b87e419bc2cd01
Ryan Nystrom <rnystrom@instagram.com>
2018-05-28 14:01:45 -0400

    passing corner tests

--- ContextMenu/SourceViewCorner.swift
+++ ContextMenu/SourceViewCorner.swift
@@ -10,7 +10,7 @@ import UIKit
 
 internal struct SourceViewCorner {
 
-    enum Position {
+    enum Position: Int {
         case topLeft
         case topRight
         case bottomLeft
---
+++ ContextMenuTests/CGRect_DominantCornerTests.swift
@@ -0,0 +1,32 @@
+//
+//  CGRect_DominantCornerTests.swift
+//  ContextMenuTests
+//
+//  Created by Ryan Nystrom on 5/28/18.
+//  Copyright © 2018 Ryan Nystrom. All rights reserved.
+//
+
+import XCTest
+@testable import ContextMenu
+
+class CGRect_DominantCornerTests: XCTestCase {
+
+    let rect = CGRect(x: 0, y: 0, width: 100, height: 100)
+
+    func test_whenCornerExists_withTopLeft() {
+        XCTAssertEqual(rect.dominantCorner(in: CGRect(x: 11, y: 11, width: 80, height: 80))?.position, .topLeft)
+    }
+
+    func test_whenCornerExists_withTopRight() {
+        XCTAssertEqual(rect.dominantCorner(in: CGRect(x: 9, y: 11, width: 80, height: 80))?.position, .topRight)
+    }
+
+    func test_whenCornerExists_withBottomLeft() {
+        XCTAssertEqual(rect.dominantCorner(in: CGRect(x: 11, y: 9, width: 80, height: 80))?.position, .bottomLeft)
+    }
+
+    func test_whenCornerExists_withBottomRight() {
+        XCTAssertEqual(rect.dominantCorner(in: CGRect(x: 9, y: 9, width: 80, height: 80))?.position, .bottomRight)
+    }
+
+}
