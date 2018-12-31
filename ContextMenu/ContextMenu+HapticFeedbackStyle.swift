703d14cef2936ea852dbdc9efa6d1a4fcd414f6e
Abdurahim Jauzee <a.jauzee@gmail.com>
2018-12-31 19:28:26 +0200

    Rename ContextMenuHapticFeedbackStyle and put the enum inside an extension to match the common style

---
+++ ContextMenu/ContextMenu+HapticFeedbackStyle.swift
@@ -0,0 +1,18 @@
+//
+//  ContextMenu+HapticFeedbackStyle.swift
+//  ContextMenu
+//
+//  Created by Abdurahim Jauzee on 31/12/2018.
+//
+
+import Foundation
+
+public extension ContextMenu {
+
+  /// Haptic Feedback types.
+  public enum ContextMenuHapticFeedbackStyle: Int {
+    case light, medium, heavy
+  }
+
+}
+
