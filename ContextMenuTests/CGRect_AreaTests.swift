522d0b96b450c13a3a0ebfa46f8663a931d28dd5
Bart Jochems <bart@declaree.com>
2019-04-30 09:37:04 +0200

    Update tests and travis configuration

--- ContextMenuTests/CGRect_AreaTests.swift
+++ ContextMenuTests/CGRect_AreaTests.swift
@@ -12,8 +12,9 @@ import XCTest
 class CGRect_AreaTests: XCTestCase {
 
     let rect = CGRect(x: 0, y: 0, width: 100, height: 100)
-    
+
     func test_whenCornersHavePadding() {
+
         XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 10, y: 10, width: 0, height: 0), position: .topLeft)), 100)
         XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 90, y: 10, width: 0, height: 0), position: .topRight)), 100)
         XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 90, y: 90, width: 0, height: 0), position: .bottomRight)), 100)
@@ -21,10 +22,10 @@ class CGRect_AreaTests: XCTestCase {
     }
 
     func test_whenCornersHaveNoPadding() {
-        XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 0, y: 0, width: 0, height: 0), position: .topLeft)), 0)
-        XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 100, y: 0, width: 0, height: 0), position: .topRight)), 0)
-        XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 100, y: 100, width: 0, height: 0), position: .bottomRight)), 0)
-        XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 0, y: 100, width: 0, height: 0), position: .bottomLeft)), 0)
+        XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 0, y: 0, width: rect.width, height: rect.height), position: .topLeft)), 0)
+        XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 100, y: 0, width: rect.width, height: rect.height), position: .topRight)), 0)
+        XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 100, y: 100, width: rect.width, height: rect.height), position: .bottomRight)), 0)
+        XCTAssertEqual(rect.area(corner: SourceViewCorner(rect: CGRect(x: 0, y: 100, width: rect.width, height: rect.height), position: .bottomLeft)), 0)
     }
-    
+
 }
